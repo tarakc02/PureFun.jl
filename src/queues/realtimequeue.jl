89ef835359b9875dca6197616437c3be441c9888
Tarak Shah <tarak@hrdag.org>
2022-08-08 08:24:46 -0700

    simplify types for realtime queues

--- src/queues/realtimequeue.jl
+++ src/queues/realtimequeue.jl
@@ -4,26 +4,17 @@ using ..PureFun
 using ..PureFun.Lazy
 using ..PureFun.Linked
 
-struct Queue{T, F, R, S} <: PureFun.PFQueue{T} where {F <: Lazy.Stream{T}, R <: Linked.List{T}, S <: Lazy.Stream{T}}
-    front::F
-    rear::R
-    schedule::S
+struct Queue{T} <: PureFun.PFQueue{T}
+    front::Lazy.Stream{T}
+    rear::Linked.List{T}
+    schedule::Lazy.Stream{T}
 end
-
-Empty{T} = Queue{T, Lazy.Empty{T}, Linked.Empty{T}, Lazy.Empty{T}} where {T}
-NonEmpty{T} = Queue{T, Lazy.NonEmpty{T}, R, S} where {R <: Linked.List{T}, S <: Lazy.Stream{T}}
 
 front(q::Queue) = q.front
 rear(q::Queue) = q.rear
 sched(q::Queue) = q.schedule
-Base.isempty(q::Empty) = true
-Base.isempty(q::NonEmpty) = false
-PureFun.empty(q::NonEmpty{T}) where T = Empty{T}()
-PureFun.empty(q::Empty) = q
-
-function Queue(f::Lazy.Stream{T}, r::Linked.List{T}, s::Lazy.Stream{T}) where {T}
-    Queue{T, typeof(f), typeof(r), typeof(s)}(f, r, s)
-end
+Base.isempty(q::Queue) = isempty(front(q))
+Base.empty(q::Queue) = Queue(empty(front(q)), empty(rear(q)), empty(sched(q)))
 
 Queue{T}() where T = Queue(Lazy.Stream{T}(), Linked.List{T}(), Lazy.Stream{T}())
 
@@ -46,9 +37,9 @@ function exec(f, r, s::Lazy.Empty)
     Queue(f_prime, empty(r), f_prime)
 end
 
-PureFun.head(q::NonEmpty) = head(front(q))
+PureFun.head(q::Queue) = head(front(q))
 PureFun.snoc(q::Queue, x) = exec(front(q), cons(x, rear(q)), sched(q))
-PureFun.tail(q::NonEmpty) = exec(tail(front(q)), rear(q), sched(q))
+PureFun.tail(q::Queue) = exec(tail(front(q)), rear(q), sched(q))
 
 function Queue(iter)
     l = Lazy.Stream(iter)
