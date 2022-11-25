94bc7ed09d91d72f7af3759c3026537f2101bfff
Tarak Shah <tarak@hrdag.org>
2022-11-25 09:41:50 -0800

    clean up of unused stuff

--- src/PureFun.jl
+++ src/PureFun.jl
@@ -12,19 +12,12 @@ include("chunk-types/chunk-interface.jl")
 include("lists-streams/chunky2.jl")
 
 ## queues
-#include("queues/batchedqueue.jl")
 include("queues/batchedqueue2.jl")
 include("queues/realtimequeue.jl")
 include("queues/bootstrappedqueue.jl")
 include("queues/hood-melville.jl")
 
-## these are also list implementations, but rely on lower-level implementations
-## of lists or queues which need to be defined first
-#include("lists-streams/ll2.jl")
-#include("lists-streams/vector-unrolled-list.jl")
-
 include("lists-streams/catenable-list.jl")
-#include("lists-streams/chunky.jl")
 
 ## Heaps
 include("heaps/pairing-heap.jl")
